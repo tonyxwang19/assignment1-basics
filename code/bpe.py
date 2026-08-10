5e7386851125b5d834ad4610f31ce94f385c8d68
Hsi-Ning-Wang <hsi-ning-wang@Hsi-Ning-Wangs-MacBook-Pro.local>
2026-08-10 17:44:28 +0800

    first working mvp

--- code/bpe.py
+++ code/bpe.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 import time
 import regex  
 import heapq
@@ -17,6 +15,9 @@ class ReversedBytes:
     
     def __lt__(self, other):
         return self.data > other.data 
+
+    def __eq__(self, other):
+        return self.data == other.data
 
 
 class BPE:
@@ -38,22 +39,14 @@ class BPE:
 
         # Initialize the frequency table.
         self.word_freq: Counter[tuple[int, ...]] = Counter() # word: frequency
+        self.words: list[list[int]] = [] # Word List
+        self.word_freqs: list[int] = [] # Word frequency list version
         self.pair_to_word: dict[tuple[int, int], dict[int, int]] = {} # pair: {word_id: frequency in this word}
+        self.pair_freq = {}
         self.pair_freq_heap: list = []
 
-    # Util Encoder and Decoder Function
-    def _decode(self, token: list[int] | int) -> list[bytes] | bytes:
-        if isinstance(token, list):
-            return [self.decoder[t] for t in token]
-        else:
-            return self.decoder[token]
-    
-    def _encode(self, token: list[bytes] | bytes) -> list[int] | int:
-        if isinstance(token, list):
-            return [self.encoder[t] for t in token]
-        else:
-            return self.encoder[token]
-        
+        self.reports = None
+
     # Find the boundaries of each chunk in the input file
     def find_chunks_bound(self) -> list[int]:
         with open(self.path, "rb") as f:
@@ -107,18 +100,103 @@ class BPE:
         return counter
 
     # Heapify Func
-    def make_heap_entry(self,pair,freq):
-        lex = (ReversedBytes(self._decode(pair[0])), ReversedBytes(self._decode(pair[1])))
+    def make_heap_entry(self, pair, freq):
+        lex = (ReversedBytes(self.decoder[pair[0]]), ReversedBytes(self.decoder[pair[1]]))
         return (-freq, lex, pair)
 
     # Merge
-
-
     def merge(self):
-        pass
+        # Find the max pair:
+        while self.pair_freq_heap:
+            neg_freq, lex, pair = heapq.heappop(self.pair_freq_heap)
+
+            if pair not in self.pair_freq:
+                continue
+
+            if -neg_freq != self.pair_freq[pair]:
+                continue
+
+            max_pair = pair
+            break
+        else:
+            return False
+
+        # Find words that contain max pair
+        affected_word_ids = list(self.pair_to_word[max_pair].keys())
+
+        # Modify the encoder/decoder, and merge history
+        new_id = len(self.decoder)
+        new_bytes = b"".join(self.decoder[p] for p in max_pair)
+ 
+        self.merges.append(
+            (self.decoder[max_pair[0]], self.decoder[max_pair[1]])
+        )
+        self.encoder[new_bytes] = new_id
+        self.decoder[new_id] = new_bytes
+
+        # Merge
+        delta: dict[tuple[int, int], int] = {}
+
+        for id in affected_word_ids:
+            word = self.words[id]
+            old_freq = self.get_pair_counts(word)
+            new_word = self.merge_word(word, max_pair, new_id)
+            new_freq = self.get_pair_counts(new_word)
+
+            weight = self.word_freqs[id]
+            affected_pairs = old_freq.keys() | new_freq.keys()
+
+            for pair in affected_pairs:
+                diff = new_freq.get(pair, 0) - old_freq.get(pair, 0)
+
+                if diff != 0:
+                    delta[pair] = delta.get(pair, 0) + diff * weight
+
+                if new_freq[pair] > 0:
+                    self.pair_to_word.setdefault(pair, {})[id] = new_freq[pair]
+                else:
+                    if pair in self.pair_to_word:
+                        self.pair_to_word[pair].pop(id, None)
+                        if not self.pair_to_word[pair]:
+                            del self.pair_to_word[pair]
+                        
+            self.words[id] = new_word
+
+        for pair, diff in delta.items():
+            new_global_freq = self.pair_freq.get(pair, 0) + diff
+
+            if new_global_freq > 0:
+                self.pair_freq[pair] = new_global_freq
+                heapq.heappush(self.pair_freq_heap, self.make_heap_entry(pair, new_global_freq))
+                
+            else:
+                self.pair_freq.pop(pair, None)
+
+
+    def get_pair_counts(self, word: list[int]) -> Counter:
+        return Counter(zip(word[:-1], word[1:]))
+
+    def merge_word(self, word: list[int], max_pair: tuple[int, int], new_id: int) -> list[int]:
+        result = []
+        i = 0
+
+        while i < len(word):
+            if (
+                i + 1 < len(word)
+                and word[i] == max_pair[0]
+                and word[i + 1] == max_pair[1]
+            ):
+                result.append(new_id)
+                i += 2
+            else:
+                result.append(word[i])
+                i += 1
+
+        return result
+
+
         
     def train(self) -> None:
-
         # Pretokenization
         start = time.perf_counter()
 
@@ -130,7 +208,7 @@ class BPE:
                 self.word_freq += result.get() 
 
         # Process Pair-Word Table
-        words = list(self.word_freq.keys())
+        words = list[tuple[int, ...]](self.word_freq.keys())
         for word_id, word in enumerate(words):
             for c in range(len(word) - 1):
                 pair = (word[c],word[c+1])
@@ -140,8 +218,13 @@ class BPE:
                     self.pair_to_word[pair].get(word_id, 0) + 1
                 )
 
+        self.words = [list(word) for word in self.word_freq.keys()]
+        self.word_freqs = list(self.word_freq.values())
+        self.word_freq.clear() # Release word_freq
+        
         # Process Pair Frequency Heap
-        self.pair_freq_heap = [self.make_heap_entry(pair, sum(self.word_freq[words[word_id]] * self.pair_to_word[pair][word_id] for word_id in self.pair_to_word[pair])) for pair in self.pair_to_word]
+        self.pair_freq = {pair: sum(self.word_freqs[word_id] * self.pair_to_word[pair][word_id] for word_id in self.pair_to_word[pair]) for pair in self.pair_to_word}
+        self.pair_freq_heap = [self.make_heap_entry(pair, freq) for pair, freq in self.pair_freq.items()]
         heapq.heapify(self.pair_freq_heap)
 
         end = time.perf_counter()
@@ -151,18 +234,30 @@ class BPE:
         # Merge
         start = time.perf_counter()
 
-        ## TODO
+        for iteration in range(self.vocab_size - len(self.decoder)):
+            self.merge()
 
         end = time.perf_counter()
         print('Phase 2: Merge Complete')
         print(f"Merge Runtime: {end - start:.4f} seconds")
-        return None
+        
+        self.reports = {
+            "vocab": self.decoder,
+            "merges": self.merges,
+        }
 
+    def report(self):
+        if not self.reports:
+            raise NotTrainedError("BPE model not trained yet")
+        return self.reports
+
+class NotTrainedError(Exception):
+    pass
 
 if __name__ == '__main__':
 
-    bpe = BPE(vocab_size=1000, input_path="tests/fixtures/tinystories_sample_5M.txt", special_tokens=['<|endoftext|>'], num_processes=6)
+    bpe = BPE(vocab_size=1000, input_path="tests/fixtures/tinystories_sample_5M.txt", special_tokens=['<|endoftext|>'], num_processes=8)
     bpe.train()
-        
-        
+    report = bpe.report()
+    # print(report)
        
